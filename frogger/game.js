12ce23c3268d5ce9b13dc9aff536279a28841857
bovice18 <beau.wood@tufts.edu>
2013-02-15 22:52:41 -0500

    Split up the draw function into different functions per element on the board.  Wrote README.txt

--- frogger/game.js
+++ frogger/game.js
@@ -1,24 +1,29 @@
+var img = new Image();
+img.src = 'assets/frogger_sprites.png';	
+
 function start_game()
 {
-	draw(190, 495, 2, false, 1, 0);
+	//locations
+	frogx = 190; frogy = 495; cars1y = 320; logs1y = 115;
+	//gameplay
+	lives = 2; over = false; level = 1; time = 0;
+	
+	drawInit(frogx, frogy, cars1y, logs1y, lives, over, level, time);	
 }
 
-function draw(frogx, frogy, lives, over, level, time)
+function drawInit(frogx, frogy, cars1y, logs1y, lives, over, level, time)
 {
 	canvas = document.getElementById("game");
 		if(canvas.getContext)
 		{
 			ctx = canvas.getContext('2d');
-			
-						
+								
 			//draw black square and blue square
 			ctx.fillStyle = "191970";
 			ctx.fillRect (0, 0, 399, 282);
 			ctx.fillStyle = "000000";
 			ctx.fillRect (0, 282, 399, 283);
 			
-			img = new Image();
-			img.src = 'assets/frogger_sprites.png';	
 			ctx.drawImage(img, 15, 13, 321, 32, 20, 10, 321, 32); //FROGGER			
 			ctx.drawImage(img, 0, 55, 399, 53, 0, 50, 399, 53); // Top Lilly Pads
 			ctx.drawImage(img, 0, 119, 399, 35, 0, 275, 399, 35); //purple sidewalks
@@ -26,21 +31,41 @@ function draw(frogx, frogy, lives, over, level, time)
 			ctx.drawImage(img, 12, 335, 19, 22, 0, 520, 18, 22); //lives
 			ctx.drawImage(img, 12, 335, 19, 22, 20, 520, 18, 22); //lives2
 			
-			ctx.font="bold 30px Microgramma";
-			ctx.fillStyle = '00FF00';
-			ctx.fillText("Level 1",55,539);
-			
-			ctx.font="bold 20px Microgramma";
-			ctx.fillText("Score: 0",0,560);
-			ctx.fillText("High Score: 0",90,560);
-			
-			ctx.drawImage(img, 13, 370, 22, 18, frogx, frogy, 22, 18); //frog			
-			ctx.drawImage(img, 47, 265, 25, 26, 165, 320, 25, 26); // vehicle 1
-			ctx.drawImage(img, 47, 265, 25, 26, 300, 320, 25, 26); //vehicle 2
-			ctx.drawImage(img, 7, 166, 178, 22, 10, 115, 178, 22); //log
+			setLevel(level);
+			setScore(0, 0);
+		
+			drawFrog(frogx, frogy);
+			drawCars1(cars1y);
+			drawLogs1(logs1y);
 		}
 		else
 		{
 			alert("Sorry, canvas not supported by your browser");
 		}
+}
+
+function drawFrog(frogx, frogy)
+{
+	ctx.drawImage(img, 13, 370, 22, 18, frogx, frogy, 22, 18); //frog
+}
+function drawCars1(ycoord)
+{
+	ctx.drawImage(img, 47, 265, 25, 26, 165, ycoord, 25, 26); // vehicle 1
+	ctx.drawImage(img, 47, 265, 25, 26, 300, ycoord, 25, 26); //vehicle 2
+}
+function drawLogs1(ycoord)
+{
+	ctx.drawImage(img, 7, 166, 178, 22, 10, logs1y, 178, 22); //log
+}
+function setLevel(level)
+{
+	ctx.font="bold 30px Microgramma";
+	ctx.fillStyle = '00FF00';
+	ctx.fillText("Level: " + level,55,539);
+}
+function setScore(score, highscore)
+{
+	ctx.font="bold 20px Microgramma";
+	ctx.fillText("Score: " + score,0,560);
+	ctx.fillText("High Score: " + highscore,90,560);
 }
