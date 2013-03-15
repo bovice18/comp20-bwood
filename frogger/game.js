db543c4dab019cac31ca837c99f40cc2b62c345a
bovice18 <beau.wood@tufts.edu>
2013-03-15 02:50:58 -0400

    Finished collision detection except for lilly pads.

--- frogger/game.js
+++ frogger/game.js
@@ -1,6 +1,5 @@
 //check for user input
 		document.addEventListener("keyup", function(event) {
-		console.log(event.keyCode);
 			if (event.keyCode == 38) {
 				frogUp();
 				}
@@ -19,12 +18,18 @@ var img = new Image();
 img.src = 'assets/frogger_sprites.png';	
 
 var frogx, frogy, cars1y, logs1y, lives, over, level, score, highscore, time, frogDir, frogMove;
-
+var frogR;
+var frogC;
 var interval;
 var frogInterval;
 var counter;
 var moving = false;
+var reset = false;
+var scored0, scored1, scored2, scored3, scored4;
+var over;
+var froglog0, froglog1, froglog2, froglog3;
 
+//Called at opening of the page
 function start_game()
 {
 	//locations
@@ -41,7 +46,330 @@ function start_game()
 	interval = setInterval(function(){redraw()},30);
 	//gamePlay();
 }
-//frogx, frogy, cars1x, logs1x, lives, over, level, time, frogDir, frogMove
+//Checks to see if the frog has landed in an empty home
+function checkScore()
+{
+	if(frogy == 74 )//&& (frogx == 187 || frogx == 271 || frogx == 355 || frogx == 103 || frogx == 19)
+	{
+		console.log("test");
+		switch(frogx)
+		{
+			case 19:
+				if(!scored0)
+				{
+					scored0 = true;
+					score = score + 50;
+				}
+				
+				break;
+			case 103:
+				if(!scored1)
+				{
+					scored1 = true;
+					score = score + 50;
+				}
+				
+				break;
+			case 187:
+				if(!scored2)
+				{
+					scored2 = true;
+					score = score + 50;
+				}
+				break;
+			case 271:
+				if(!scored3)
+				{
+					scored3 = true;
+					score = score + 50;
+				}
+				break;
+			case 355:
+				if(!scored4)
+				{
+					scored4 = true;
+					score = score + 50;
+				}
+				break;
+		}
+		
+		resetFrog()
+	}
+}
+function resetFrog()
+{
+	frogx = 187; frogy = 500;
+}
+function checkCollision()
+{
+//console.log(frogy);
+	if(frogDir == 0 || frogDir == 3)
+	{
+		frogR = frogx + 18;
+		frogC = frogx + 9;
+	}
+	else if(frogDir == 1 || frogDir == 2)
+	{
+		frogR = frogx + 23;
+		frogC = frogx + 12
+	}
+	 
+	switch(frogy)
+	{
+		case 500:
+			//safe
+			break;
+		case 459:
+			collision0();
+			break;
+		case 414:
+			collision1();
+			break;
+		case 369:
+			collision2();
+			break;
+		case 324:
+			collision3();
+			break;
+		case 289:
+			//safe
+			break;
+		case 254:
+			collision4();
+			break;
+		case 209:
+			collision5();
+			break;
+		case 164:
+			collision6();
+			break;
+		case 119:
+			collision7();
+			break;
+		case 74:
+			collision8();
+			break;
+	}
+}
+function collision0()
+{
+	if((frogx > cars4x && frogx < (cars4x + 30)) || (frogR > cars4x && frogR < (cars4x + 30)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars4x+135) && frogx < (cars4x + 165)) || (frogR > (cars4x+135) && frogR < (cars4x + 165)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars4x+270) && frogx < (cars4x + 300)) || (frogR > (cars4x+270) && frogR < (cars4x + 300)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars4x+405) && frogx < (cars4x + 435)) || (frogR > (cars4x+405) && frogR < (cars4x + 435)))
+	{
+		COLLISION();
+	}
+	
+}
+function collision1()
+{
+	if((frogx > cars3x && frogx < (cars3x + 30)) || (frogR > cars3x && frogR < (cars3x + 30)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars3x+135) && frogx < (cars3x + 160)) || (frogR > (cars3x+135) && frogR < (cars3x + 160)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars3x+270) && frogx < (cars3x + 295)) || (frogR > (cars3x+270) && frogR < (cars3x + 295)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars3x+405) && frogx < (cars3x + 430)) || (frogR > (cars3x+405) && frogR < (cars3x + 430)))
+	{
+		COLLISION();
+	}
+}
+function collision2()
+{
+	if((frogx > cars2x && frogx < (cars2x + 30)) || (frogR > cars2x && frogR < (cars2x + 30)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars2x+135) && frogx < (cars2x + 160)) || (frogR > (cars2x+135) && frogR < (cars2x + 160)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars2x+270) && frogx < (cars2x + 295)) || (frogR > (cars2x+270) && frogR < (cars2x + 295)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars2x+405) && frogx < (cars2x + 430)) || (frogR > (cars2x+405) && frogR < (cars2x + 430)))
+	{
+		COLLISION();
+	}
+}
+function collision3()
+{
+	if((frogx > cars1x && frogx < (cars1x + 30)) || (frogR > cars1x && frogR < (cars1x + 30)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars1x+135) && frogx < (cars1x + 160)) || (frogR > (cars1x+135) && frogR < (cars1x + 160)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars1x+270) && frogx < (cars1x + 295)) || (frogR > (cars1x+270) && frogR < (cars1x + 295)))
+	{
+		COLLISION();
+	}
+	else if((frogx > (cars1x+405) && frogx < (cars1x + 430)) || (frogR > (cars1x+405) && frogR < (cars1x + 430)))
+	{
+		COLLISION();
+	}
+}
+function collision4()
+{
+	if(frogC > logs4x && frogC < (logs4x + 118))
+	{
+		froglog0 = true;;
+	}
+	else if(frogC > (logs4x+200) && frogC < (logs4x + 318))
+	{
+		froglog0 = true;
+	}
+	else if(frogC > (logs4x+400) && frogC < (logs4x + 518))
+	{
+		froglog0 = true;
+	}
+	else if(frogC > (logs4x+600) && frogC < (logs4x + 718))
+	{
+		froglog0 = true;
+	}
+	else if(frogC > (logs4x+800) && frogC < (logs4x + 918))
+	{
+		froglog0 = true;
+	}
+	else
+	{
+		COLLISION();
+	}
+}
+function collision5()
+{
+	if(frogC > logs3x && frogC < (logs3x + 84))
+	{
+		froglog1 = true;;
+	}
+	else if(frogC > (logs3x+150) && frogC < (logs3x + 234))
+	{
+		froglog1 = true;
+	}
+	else if(frogC > (logs3x+300) && frogC < (logs3x + 384))
+	{
+		froglog1 = true;
+	}
+	else if(frogC > (logs3x+450) && frogC < (logs3x + 534))
+	{
+		froglog1 = true;
+	}
+	else if(frogC > (logs3x+600) && frogC < (logs3x + 684))
+	{
+		froglog1 = true;
+	}
+	else
+	{
+		COLLISION();
+	}
+}
+function collision6()
+{
+
+	if(frogC > logs2x && frogC < (logs2x + 118))
+	{
+		froglog2 = true;
+	}
+	else if(frogC > (logs2x+150) && frogC < (logs2x + 268))
+	{
+		froglog2 = true;
+	}
+	else if(frogC > (logs2x+300) && frogC < (logs2x + 418))
+	{
+		froglog2 = true;
+	}
+	else if(frogC > (logs2x+450) && frogC < (logs2x + 568))
+	{
+		froglog2 = true;
+	}
+	else if(frogC > (logs2x+600) && frogC < (logs2x + 718))
+	{
+		froglog2 = true;
+	}
+	else
+	{
+		COLLISION();
+	}
+}
+function collision7()
+{
+	console.log(frogC);
+	console.log(logs1x);
+	//clearInterval(interval);
+	if(frogC > logs1x && frogC < (logs1x + 178))
+	{
+		froglog3 = true;
+	}
+	else if(frogC > (logs1x+200) && frogC < (logs1x + 378))
+	{
+		froglog3 = true;
+	}
+	else if(frogC > (logs1x+400) && frogC < (logs1x + 578))
+	{
+		froglog3 = true;
+	}
+	else if(frogC > (logs1x+600) && frogC < (logs1x + 778))
+	{
+		froglog3 = true;
+	}
+	else if(frogC > (logs1x+800) && frogC < (logs1x + 978))
+	{
+		froglog3 = true;
+	}
+	else
+	{
+		COLLISION();
+	}
+}
+function collision8()
+{
+
+}
+function collision9()
+{
+
+}
+function COLLISION()
+{
+	
+	if(lives == 0)
+	{
+		clearInterval(interval);
+		gameOver();	
+	}
+	resetFrog();
+	lives--;
+}
+function gameOver()
+{
+	over = true;
+	ctx.fillStyle = "000000";
+	ctx.fillRect (0, 0, 399, 565);
+	
+	ctx.font="bold 30px Microgramma";
+	ctx.fillStyle = '00FF00';
+	ctx.fillText("GAME OVER",112, 275);
+}
+//Called very often, calls all other draw functions to draw the entire canvas
 function draw()
 {
 	canvas = document.getElementById("game");
@@ -64,13 +392,37 @@ function draw()
 			setLevel();
 			setScore();
 		
-			drawFrog();
+			
 			drawCars1();
 			drawCars2();
 			drawCars3();
 			drawCars4();
 			
 			drawLogs1(); drawLogs2(); drawLogs3(); drawLogs4();
+			
+			drawFrog();
+			
+			if(scored0)
+			{
+				ctx.drawImage(img, 13, 370, 22, 18, 19, 74, 22, 18); //frog
+			}
+			if(scored1)
+			{
+				ctx.drawImage(img, 13, 370, 22, 18, 103, 74, 22, 18); //frog
+			}
+			if(scored2)
+			{
+				ctx.drawImage(img, 13, 370, 22, 18, 187, 74, 22, 18); //frog
+			}
+			if(scored3)
+			{
+				ctx.drawImage(img, 13, 370, 22, 18, 271, 74, 22, 18); //frog
+			}
+			if(scored4)
+			{
+				ctx.drawImage(img, 13, 370, 22, 18, 355, 74, 22, 18); //frog
+			}
+			
 		}
 		else
 		{
@@ -78,6 +430,7 @@ function draw()
 		}
 }
 
+//Decides which frog to draw depending on direction and whether the frog is moving or not
 function drawFrog()
 {
 	if(frogDir == 0) //forward
@@ -126,6 +479,15 @@ function drawFrog()
 	}
 	
 }
+
+function clearFrogLog()
+{
+	froglog0 = false;
+	froglog1 = false;
+	froglog2 = false;
+	froglog3 = false;
+}
+//Functions to draw the 4 rows of cars:
 function drawCars1()
 {
 	if(cars1x > 115)
@@ -175,6 +537,7 @@ function drawCars4()
 
 }
 
+//Functions to draw the 4 rows of logs:
 function drawLogs1()
 {
 	if(logs1x == -237)
@@ -223,18 +586,23 @@ function drawLogs4()
 	ctx.drawImage(img, 6, 197, 118, 23, logs4x + 800, 250, 118, 23); //log
 
 }
+
+
+
 function setLevel()
 {
 	ctx.font="bold 30px Microgramma";
 	ctx.fillStyle = '00FF00';
 	ctx.fillText("Level: " + level,55,539);
 }
+
 function setScore()
 {
 	ctx.font="bold 20px Microgramma";
 	ctx.fillText("Score: " + score,0,560);
 	ctx.fillText("High Score: " + highscore,120,560);
 }
+
 function drawLives()
 {
 	if(lives == 2)
@@ -249,15 +617,37 @@ function drawLives()
 	
 	
 }
+
+//function is called every 30ms for animation
 function redraw()
 {
 	//cars
 	cars1x = cars1x+1.5; cars2x = cars2x-1.5; cars3x = cars3x+0.75; cars4x = cars4x-2; 
 	//logs
 	logs1x = logs1x-1;  logs2x = logs2x+1.5; logs3x = logs3x-0.75; logs4x = logs4x+2;
+	if(froglog0)
+	{
+		frogx = frogx+2;;
+	}
+	else if(froglog1)
+	{
+		frogx = frogx-0.75;
+	}
+	else if(froglog2)
+	{
+		frogx = frogx+1.5;
+	}
+	else if(froglog3)
+	{
+		frogx = frogx-1;
+	}
 	draw();
+	clearFrogLog();
+	checkCollision();
 
 }
+
+
 function frogUp()
 {
 	frogDir = 0;
@@ -303,8 +693,9 @@ function frogUp()
 		{
 			moving = true;
 			frogInterval = setInterval(function(){moveUp(45)},3);
-		}	}
-	
+		}	
+	}
+	score = score + 10;	
 }
 function moveUp(y)
 {
@@ -315,11 +706,10 @@ function moveUp(y)
 		clearInterval(frogInterval);
 		frogMove = false;
 		moving = false;
+		checkScore();
 	}
 	draw();
 }
-
-
 
 
 
